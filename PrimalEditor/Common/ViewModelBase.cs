d8889a7351051855ac2d82f579676e049427a4e3
Cristian Kovacs <cris.kovacs14@gmail.com>
2021-05-27 23:20:11 -0400

    First real merge into git with all the components so that I could work on it outside of my laptop possibly.

---
+++ PrimalEditor/Common/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PrimalEditor
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyNane)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyNane));
+        }
+    }
+}
