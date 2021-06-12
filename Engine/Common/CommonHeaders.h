5d46043267a671f477f7fc8558ec7737f1a8da95
Cristian Kovacs <cris.kovacs14@gmail.com>
2021-06-11 21:07:18 -0400

    Started the actual engine. Working on the entity component where we will break down entities down to ids of the entity itself, to the different components of the entities. Using a lot of bit-math to set this all up.

---
+++ Engine/Common/CommonHeaders.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// C/C++ headers
+#include <stdint.h>
+#include <assert.h>
+
+// Common headers
+#include "PrimitiveTypes.h"
---
+++ Engine/Components/ComponentsCommon.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "ComponentsCommon.h"
+#include "Id.h"
+
+namespace primal::game_entity
+{
+	DEFINE_TYPED_ID(entity_id);
+}
